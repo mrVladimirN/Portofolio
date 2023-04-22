54df627e1aad1f1b835a92994f12fac2902c0b3e
mrVladimirN <vladimir.nitu.business@outlook.com>
2023-04-21 23:55:45 -0700

    tailwind configuration

--- portofolio/src/pages/_app.tsx
+++ portofolio/src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        </meta>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
+
+      <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
+        <Component {...pageProps} />
+     
+      </main>
+    </>
+  );
 }
--- portofolio/tailwind.config.js
+++ portofolio/tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  mode: "jit",
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
       },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96",
+        primaryDark: "#58E6D9",
+      },
+      backgroundImage: {
+        circularLight:
+          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px,#f5f5f5  100px)",
+        circularDark:
+          "repeating-radial-gradient(rgba(255,255,255,0.4) 2px, #1b1b1b 5px,#1b1b1b  100px)",
+      },
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+      xs: { max: "479px" },
     },
   },
   plugins: [],
-}
+};
